2dd2cf4ca81ab31726e3e4211b589a7d43515841
jhssong <jhssong02@gmail.com>
2025-04-11 11:52:05 +0900

    feat: Add GitHubAuth schemas

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import RedirectResponse
-from src.auth import service, config
+from src.auth import service, config, schemas
 
 router = APIRouter(prefix="/auth/github", tags=["GitHub OAuth"])
 
 GITHUB_OAUTH_AUTHORIZE_URL = "https://github.com/login/oauth/authorize"
 
-@router.get("/login")
+@router.get("/login", summary="Redirect to GitHub login page")
 def login_with_github():
     github_url = (
         f"{GITHUB_OAUTH_AUTHORIZE_URL}"
@@ -16,7 +16,7 @@ def login_with_github():
     )
     return RedirectResponse(github_url)
 
-@router.get("/callback")
+@router.get("/callback", response_model=schemas.GitHubAuthResponse, summary="Callback for GitHub login")
 async def github_callback(request: Request):
     code = request.query_params.get("code")
     if not code:
@@ -29,14 +29,15 @@ async def github_callback(request: Request):
     user = await service.get_github_user(access_token)
     repos = await service.get_user_repos(access_token)
 
-    return {
-        "user": user,
-        "repositories": [
-            {
-                "name": repo["name"],
-                "private": repo["private"],
-                "url": repo["html_url"],
-                "description": repo["description"],
-            } for repo in repos
+    return schemas.GitHubAuthResponse(
+        user=user,
+        repositories=[
+            schemas.RepoInfo(
+                name=repo["name"],
+                private=repo["private"],
+                url=repo["html_url"],
+                description=repo.get("description"),
+            )
+            for repo in repos
         ]
-    }
+    )
