bf31b7c666e0350ea6d46b47930e4d214de0ea1d
jhssong <jhssong02@gmail.com>
2025-04-13 01:32:26 +0900

    feat: Setup database

--- src/database.py
+++ src/database.py
@@ -3,14 +3,18 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DB_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DB_URL, pool_pre_ping=True)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+
 def get_db():
+    """
+    Create SQLAlchemy Sessoin
+    """
     db = SessionLocal()
     try:
         yield db
