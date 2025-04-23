6c75b0ee35854dff936cba12bdc8f393a160c2c0
jhssong <jhssong02@gmail.com>
2025-04-24 00:05:36 +0900

    feat: Implement user search function

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from src.config import FRONTEND_URL
 # Import routers
 from auth.router import router as auth_router
 from project.router import router as project_router
+from user.router import router as user_router
 
 
 app = FastAPI(
@@ -40,3 +41,4 @@ def read_root():
 # Include routers
 app.include_router(auth_router)
 app.include_router(project_router)
+app.include_router(user_router)
--- src/user/service.py
+++ src/user/service.py
@@ -3,10 +3,11 @@ from src.exceptions.definitions import UserAlreadyExist
 from src.models import User
 from user.schemas import UserReq, UserRes
 from user import repository
+from src.models import User
 from auth.util.redis import get_token_from_redis
 
 
-async def create_user(db: Session, user_req: UserReq) -> User:
+async def create_user(db: Session, user_req: UserReq) -> UserRes:
     """
     Create new user
     """
@@ -16,7 +17,7 @@ async def create_user(db: Session, user_req: UserReq) -> User:
 
     github_access_token = await get_token_from_redis("github_oauth", user_req.github_id)
 
-    user = User(
+    new_user = User(
         name=user_req.name,
         discord_id=user_req.discord_id,
         github_id=user_req.github_id,
@@ -26,4 +27,18 @@ async def create_user(db: Session, user_req: UserReq) -> User:
         career=user_req.career,
     )
 
-    return repository.create_user(db, user)
+    saved_user = await create_user(db, new_user)
+    user_res = UserRes.model_validate(saved_user)
+
+    return user_res
+
+
+def search_users_by_name(db: Session, user_name: str):
+    users = db.query(User).filter(User.username.ilike(f"%{user_name}%")).all()
+    user_res_list = []
+
+    for user in users:
+        user_res = UserRes.model_validate(user)
+        user_res_list.append(user_res)
+
+    return user_res_list
