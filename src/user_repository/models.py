9a7790cc46377794c2e93337d16eca5f0ec59f50
jhssong <jhssong02@gmail.com>
2025-05-05 21:58:13 +0900

    feat: Add user_repository schemas

--- src/user_repository/models.py
+++ src/user_repository/models.py
@@ -7,6 +7,6 @@ class UserRepository(Base):
     __tablename__ = "user_repository"
 
     user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
-    repo_name = Column(String(255))
+    repo_full_name = Column(String(255))
 
     user = relationship("User", back_populates="repos")
