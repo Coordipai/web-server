0a5d64de8e246bc713fa046a9bb02a1eb9fb1f20
junhyung85920 <junhyung85920@gmail.com>
2025-05-05 18:05:06 +0900

    feat: Implement db access functions for issue reschedule request

---
+++ src/issue_reschedule_request/repository.py
@@ -0,0 +1,58 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import SQLAlchemyError
+from src.response.error_definitions import SQLError
+from src.config.logger_config import add_daily_file_handler, setup_logger
+from src.models import IssueRescheduleRequest
+
+logger = setup_logger(__name__)
+add_daily_file_handler(logger)
+
+
+def create_issue_reschedule_request(db: Session, issue_reschedule_request: IssueRescheduleRequest) -> IssueRescheduleRequest:
+    try:
+        db.add(issue_reschedule_request)
+        db.commit()
+        db.refresh(issue_reschedule_request)
+        return issue_reschedule_request
+    except SQLAlchemyError as e:
+        logger.error(f"Database error during issue reschedule request creation: {e}")
+        db.rollback()
+        raise SQLError()
+    
+
+def find_issue_reschedule_request_by_id(db: Session, request_id: int) -> IssueRescheduleRequest | None:
+    try:
+        result = db.execute(select(IssueRescheduleRequest).filter(IssueRescheduleRequest.id == request_id))
+        return result.scalars().first()
+    except NoResultFound:
+        return None
+    except SQLAlchemyError as e:
+        logger.error(f"Database error: {e}")
+        db.rollback()
+        raise SQLError()
+    
+    
+def find_issue_reschedule_request_by_issue_num(db: Session, issue_num: int) -> IssueRescheduleRequest | None:
+    try:
+        result = db.execute(select(IssueRescheduleRequest).filter(IssueRescheduleRequest.issue_num == issue_num))
+        return result.scalars().first()
+    except NoResultFound:
+        return None
+    except SQLAlchemyError as e:
+        logger.error(f"Database error: {e}")
+        db.rollback()
+        raise SQLError()
+    
+
+def find_issue_reschedule_requests_by_user_id(db: Session ,user_id: int) -> list[IssueRescheduleRequest] | None:
+    try:
+        result = db.execute(select(IssueRescheduleRequest).filter(IssueRescheduleRequest.assignee == user_id))
+        return result.scalars().all()
+    except NoResultFound:
+        return None
+    except SQLAlchemyError as e:
+        logger.error(f"Database error: {e}")
+        db.rollback()
+        raise SQLError()
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,6 @@
 from src.user.models import User
 from src.project.models import Project
 from src.project_user.models import ProjectUser
+from src.issue_reschedule_request.models import IssueRescheduleRequest
 
-__all__ = ["User", "Project", "ProjectUser"]
+__all__ = ["User", "Project", "ProjectUser", "IssueRescheduleRequest"]
