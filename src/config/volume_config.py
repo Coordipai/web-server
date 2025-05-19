da30f2b50ee600854be7fb77c07b3cd4549b39eb
jhssong <jhssong02@gmail.com>
2025-05-19 13:26:56 +0900

    hotfix: Remove all files in design_docs not directory itself

--- src/config/volume_config.py
+++ src/config/volume_config.py
@@ -7,8 +7,10 @@ def clear_design_docs():
     if os.path.exists(base_path):
         for name in os.listdir(base_path):
             path = os.path.join(base_path, name)
-            if os.path.isdir(path):
-                shutil.rmtree(path)
-            else:
-                os.remove(path)
-        shutil.rmtree(base_path)
+            try:
+                if os.path.isdir(path):
+                    shutil.rmtree(path)
+                else:
+                    os.remove(path)
+            except Exception as e:
+                print(f"Failed to delete {path}: {e}")
